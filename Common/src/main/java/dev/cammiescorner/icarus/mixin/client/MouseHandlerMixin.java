cf086ee68a39f2e84f3eb72964b335ce25578e3c
Up <10714589+UpcraftLP@users.noreply.github.com>
2026-08-24 17:54:40 +0200

    change mixin to use WrapOperation (fixes #155)

--- Common/src/main/java/dev/cammiescorner/icarus/mixin/client/MouseHandlerMixin.java
+++ Common/src/main/java/dev/cammiescorner/icarus/mixin/client/MouseHandlerMixin.java
@@ -1,33 +1,27 @@
 package dev.cammiescorner.icarus.mixin.client;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import dev.cammiescorner.icarus.client.ClientPlayerFallbackValues;
 import dev.cammiescorner.icarus.util.IcarusHelper;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.MouseHandler;
-import org.spongepowered.asm.mixin.Final;
+import net.minecraft.client.player.LocalPlayer;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 @Mixin(MouseHandler.class)
 public class MouseHandlerMixin {
 
-    @Shadow
-    @Final
-    private Minecraft minecraft;
-
-    @ModifyArg(method = "turnPlayer", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/player/LocalPlayer;turn(DD)V"), index = 0)
-    public double changeLookDirectionX(double x) {
-        var player = minecraft.player;
-
-        // we inject inside a null check so this is just to make the IDE happy
-        assert player != null;
-
+    @WrapOperation(method = "turnPlayer", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/player/LocalPlayer;turn(DD)V"))
+    private void changelookDirectionX(LocalPlayer player, double x, double y, Operation<Void> op) {
+        double actualX;
         if ((player.getXRot() > 90 || player.getXRot() < -90) && ClientPlayerFallbackValues.canClientLoopDeLoop(player) && player.isFallFlying() && IcarusHelper.hasWings(player) && IcarusHelper.getConfigValues(player).canLoopDeLoop()) {
-            return -x;
+            actualX = -x;
+        }
+        else {
+            actualX = x;
         }
 
-        return x;
+        op.call(player, actualX, y);
     }
 }
